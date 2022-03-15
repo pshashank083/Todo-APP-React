f9f3b8f079d623960035cccde16781fe737e41bc
Shashank Pandey <pshashank083@gmail.com>
2022-03-15 20:27:46 +0530

    final code

---
+++ todo-list/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import TodoForm from './Components/TodoForm';
+import TodoList from './Components/TodoList';
+
+function App() {
+  return (
+    <div className="todo">
+        <TodoList />
+    </div>
+  );
+}
+
+export default App;
